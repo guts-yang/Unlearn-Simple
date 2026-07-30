106d1c246f623683e8f27a90e3916e0e83909041
guts-yang <578165807@qq.com>
2026-07-30 16:33:33 +0800

    fix: 完善机群配置

--- TOFU/dataloader.py
+++ TOFU/dataloader.py
@@ -26,7 +26,7 @@ import tqdm
 
 
 class CustomTrainer(Trainer):
-    def compute_loss(self, model, inputs, return_outputs=False):
+    def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
         input_ids, labels, attention_mask = inputs
         outputs = model(input_ids,labels=labels, attention_mask=attention_mask)
         loss = outputs.loss
@@ -145,7 +145,7 @@ class CustomTrainerForgetting(Trainer):
 
         return model
     
-    def compute_loss(self, model, inputs, return_outputs=False):
+    def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
         if self.loss_type == "grad_ascent":
             forget_inputs, retain_inputs = inputs
             input_ids, labels, attention_mask = forget_inputs
@@ -614,7 +614,7 @@ class CustomTrainerRetraining(Trainer):
             dataloader_params["worker_init_fn"] = seed_worker
         return self.accelerator.prepare(DataLoader(train_dataset, **dataloader_params))
 
-    def compute_loss(self, model, inputs, return_outputs=False):
+    def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
         input_ids, labels, attention_mask = inputs
         outputs = model(input_ids,labels=labels, attention_mask=attention_mask)
         loss = outputs.loss
